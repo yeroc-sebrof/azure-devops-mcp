4462e6c249359f618a483b1da9ab70198ade79d2
Vinayak Joshi <vinayakjoshi+microsoft@microsoft.com>
2025-06-13 16:45:40 +0530

    comment2

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ async function main() {
   );
 
   const transport = new StdioServerTransport();
-  console.log("Azure DevOps MCP Server version : " + server.version);
+  console.log("Azure DevOps MCP Server version : " + packageVersion);
   await server.connect(transport);
 }
 
